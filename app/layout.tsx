5fae7442a543eb25b0d5a2b181a477be7d6e18b9
Developer <>
2025-08-10 20:08:05 +0700

    add smartlook

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,24 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const smartlookScript = `
+    window.smartlook||(function(d) {
+            
+            var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+            var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+            c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+            })(document);
+            
+            smartlook('init', '03d3d997919105d2bc7de0dc9e564424b2c7d55b', { region: 'eu' });
+  `;
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{ __html: smartlookScript }}
+          async
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
