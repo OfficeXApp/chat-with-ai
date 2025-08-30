fc3be83023c5556e6632384e9b49c3999452362e
Developer <>
2025-08-30 17:06:23 +0700

    hide header mode

--- app/assistant.tsx
+++ app/assistant.tsx
@@ -31,6 +31,13 @@ import { useEffect, useState } from "react";
 
 export const Assistant = () => {
   const [apiKey, setApiKey] = useState("");
+  const [isPopPanelMode, setIsPopPanelMode] = useState(false);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const is_pop_panel_mode = params.get("pop_panel_mode");
+    setIsPopPanelMode(!!is_pop_panel_mode);
+  }, []);
 
   useEffect(() => {
     const API_KEY_GEMINI_LOCAL_STORAGE = localStorage.getItem(
@@ -66,54 +73,56 @@ export const Assistant = () => {
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-            <SidebarTrigger />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">OfficeX</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>{Locale.TopBar.NewChat}</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-            <Popover
-              title={<span>Bring Your Own API Keys</span>}
-              content={
-                <div>
-                  <span>
-                    Get your free API Key from Google Gemini -{" "}
-                    <a
-                      href="https://aistudio.google.com/app/apikey"
-                      target="_blank"
-                    >
-                      Learn more
-                    </a>
-                  </span>
-                  <Input
-                    type="password"
-                    placeholder="Enter your Gemini API key"
-                    value={apiKey}
-                    onChange={(e) => updateApiKey(e.target.value)}
-                    style={{ marginTop: 8 }}
-                  />
-                </div>
-              }
-              trigger="click"
-            >
-              <Button
-                style={{
-                  // align to right
-                  marginLeft: "auto",
-                }}
+          {!isPopPanelMode && (
+            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+              <SidebarTrigger />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="#">OfficeX</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>{Locale.TopBar.NewChat}</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+              <Popover
+                title={<span>Bring Your Own API Keys</span>}
+                content={
+                  <div>
+                    <span>
+                      Get your free API Key from Google Gemini -{" "}
+                      <a
+                        href="https://aistudio.google.com/app/apikey"
+                        target="_blank"
+                      >
+                        Learn more
+                      </a>
+                    </span>
+                    <Input
+                      type="password"
+                      placeholder="Enter your Gemini API key"
+                      value={apiKey}
+                      onChange={(e) => updateApiKey(e.target.value)}
+                      style={{ marginTop: 8 }}
+                    />
+                  </div>
+                }
+                trigger="click"
               >
-                Settings
-              </Button>
-            </Popover>
-          </header>
+                <Button
+                  style={{
+                    // align to right
+                    marginLeft: "auto",
+                  }}
+                >
+                  Settings
+                </Button>
+              </Popover>
+            </header>
+          )}
           <Thread />
         </SidebarInset>
       </SidebarProvider>
